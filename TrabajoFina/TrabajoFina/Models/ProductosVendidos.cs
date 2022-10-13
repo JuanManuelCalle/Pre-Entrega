e1a90c179246ea607b6c19018340a5d5179577b4
JuanManuelCalle <juancalle478@gmail.com>
2022-10-13 10:24:29 -0500

    Se sube proyecto en rama Master
    Se sube pre entrega proyecto final en la rama Master

---
+++ TrabajoFina/TrabajoFina/Models/ProductosVendidos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoFina.Models
+{
+    public class ProductosVendidos
+    {
+        public int Id { get; set; }
+        public int IdUsuarioVenta { get; set; }
+        public int IdProducto { get; set; }
+        public int IdVenta { get; set; }
+
+        public ProductosVendidos()
+        {
+            Id = 0;
+            IdUsuarioVenta = 0;
+            IdProducto = 0;
+            IdVenta = 0;
+        }
+    }
+}
